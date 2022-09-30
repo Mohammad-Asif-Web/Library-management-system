d0a49a13ce6f8f1f6c28b9cfd282cfa9520a295c
Muhammad Asif <alexasifhhossain@gmail.com>
2022-09-30 18:42:18 +0600

    error solved

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 <?php
    session_start(); 
+   error_reporting(0);
    // session_destroy();
    if (!(isset($_SESSION['auth']) && $_SESSION['auth'] === true)) {
    	header("Location: index.php");
@@ -8,10 +9,12 @@
    else {
     	$admin = $_SESSION['admin'];
    }
+
    require 'includes/db-inc.php';
    include "includes/header.php";
-   
+   include "includes/nav.php";
    ?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -19,19 +22,20 @@
    </head>
    <body>
       <div class="container">
-         <?php include "includes/nav.php"; ?>
+         
          <!-- navbar ends -->
          <!-- info alert -->
          <div class="alert alert-warning col-lg-7 col-md-12 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-0 col-sm-offset-1 col-xs-offset-0" style="margin-top:70px">
             <h4 class="center-block"><strong>ADMIN DASHBOARD</strong> </h4>
          </div>
          <div class="row">
-            <a href="bookstable.php"><button class="btn btn-success col-lg-3 col-md-4 col-sm-11 col-xs-11 button" style="margin-left: 15px;margin-bottom: 5px"><span class="glyphicon"></span> All Books </button></a>
+            <a href="bookslist.php"><button class="btn btn-success col-lg-3 col-md-4 col-sm-11 col-xs-11 button" style="margin-left: 15px;margin-bottom: 5px"><span class="glyphicon"></span> All Books </button></a>
             <a href="viewstudents.php"><button class="btn btn-success col-lg-3 col-md-4 col-sm-11 col-xs-11 button" style="margin-left: 15px;margin-bottom: 5px"><span class="glyphicon"></span> Students List</button></a>
             <a href="borrowedbooks.php"><button class="btn btn-success col-lg-3 col-md-4 col-sm-11 col-xs-11 button" style="margin-left: 15px;margin-bottom: 5px"><span class="glyphicon"></span> Borrowed Books</button></a>      
          </div>
       </div>
       </div>
+      
       <script type="text/javascript" src="js/jquery.js"></script>
       <script type="text/javascript" src="js/bootstrap.js"></script>	
    </body>
