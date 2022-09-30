9d543c603c67ae51d55a811d07c146ff8f7d2c0d
Muhammad Asif <alexasifhhossain@gmail.com>
2022-09-30 15:54:43 +0600

    footer script added

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<footer class="primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2022 Library Management System | Powered by <a href="https://www.linkedin.com/in/mohammadasif10/">Muhammad Asif</a></span>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>	
+<script type="text/javascript">
+   function Delete() {
+               return confirm('Would you like to delete the User?');
+           }
+</script>
+</body>
+</html>
